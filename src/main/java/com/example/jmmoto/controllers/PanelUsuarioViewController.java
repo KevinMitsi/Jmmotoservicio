9ec1927fa6327ff1399101caa670d9ddae0184e6
anaSDuque <112828439+anaSDuque@users.noreply.github.com>
2023-10-09 10:20:31 -0500

    Ahora la aplicación le muestra al cleinte el catálogo y se colocó un apartado de repuestos para que proximamente se puedan agregar

--- src/main/java/com/example/jmmoto/controllers/PanelUsuarioViewController.java
+++ src/main/java/com/example/jmmoto/controllers/PanelUsuarioViewController.java
@@ -5,6 +5,7 @@ import com.example.jmmoto.model.persona.Cliente;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.FlowPane;
@@ -12,6 +13,7 @@ import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
 public class PanelUsuarioViewController {
+    public ScrollPane scrollPane;
     MainJm main;
     Cliente clienteLogeado;
     @FXML
@@ -25,23 +27,49 @@ public class PanelUsuarioViewController {
     private void onServiciosButtonClick() {
         // Limpiar el contentPane antes de agregar nuevos elementos
         contentPane.getChildren().clear();
+        // Ejemplo de cómo crear una publicación personalizada
+        String imagenPath1 = "/com/example/jmmoto/kilometraje.png";
+        String titulo1 = "Revisión de Kilometraje";
+        String descripcion1 = "Descripción personalizada 1";
+        VBox publicationPane1 = crearPublicacion(imagenPath1, titulo1, descripcion1);
+        contentPane.getChildren().add(publicationPane1);
+        String imagenPath2 = "/com/example/jmmoto/mantnimiento.png";
+        String titulo2 = "Mantenimiento";
+        String descripcion2 = "Descripción personalizada 1";
+        VBox publicationPane2 = crearPublicacion(imagenPath2, titulo2, descripcion2);
+        contentPane.getChildren().add(publicationPane2);
+        String imagenPath3 = "/com/example/jmmoto/peritaje.png";
+        String titulo3 = "Peritaje";
+        String descripcion3 = "Descripción personalizada 1";
+        VBox publicationPane3 = crearPublicacion(imagenPath3, titulo3, descripcion3);
+        contentPane.getChildren().add(publicationPane3);
+        String imagenPath4 = "/com/example/jmmoto/serviciosRapidos.png";
+        String titulo4 = "Revisión de Kilometraje";
+        String descripcion4 = "Descripción personalizada 1";
+        VBox publicationPane4 = crearPublicacion(imagenPath4, titulo4, descripcion4);
+        contentPane.getChildren().add(publicationPane4);
 
-        // Crear las publicaciones de servicios
-        for (int i = 1; i <= 5; i++) {
-            // Crear elementos de la publicación
-            ImageView imageView = new ImageView(new Image("ruta_de_la_imagen_" + i + ".png"));
-            Label titleLabel = new Label("Título del Servicio " + i);
-            Label descriptionLabel = new Label("Descripción del Servicio " + i);
-            Button assignButton = new Button("Asignar cita");
-
-            // Crear el panel de la publicación
-            VBox publicationPane = new VBox(imageView, titleLabel, descriptionLabel, assignButton);
-            publicationPane.getStyleClass().add("publication-pane");
-
-            // Agregar la publicación al contentPane
-            contentPane.getChildren().add(publicationPane);
-        }
+        // Configurar el contenido del ScrollPane
+        scrollPane.setMaxSize(400, 400);
+        scrollPane.setContent(contentPane);
     }
+
+    private VBox crearPublicacion(String imagenPath, String titulo, String descripcion) {
+        // Crear elementos de la publicación
+        ImageView imageView = new ImageView(new Image(getClass().getResourceAsStream(imagenPath)));
+        imageView.setFitHeight(100);  // Establecer la altura deseada
+        imageView.setPreserveRatio(true);  // Mantener la proporción de la imagen
+        Label titleLabel = new Label(titulo);
+        Label descriptionLabel = new Label(descripcion);
+        Button assignButton = new Button("Asignar cita");
+
+        // Crear el panel de la publicación
+        VBox publicationPane = new VBox(imageView, titleLabel, descriptionLabel, assignButton);
+        publicationPane.getStyleClass().add("publication-pane");
+
+        return publicationPane;
+    }
+
 
     @FXML
     private void onRepuestosButtonClick() {
